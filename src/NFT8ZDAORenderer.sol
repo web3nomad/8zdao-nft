70cb4019c017fefdd1a5e1958e362e304ef52c57
web3nomad.eth <me@web3nomad.eth>
2022-06-05 18:18:16 +0800

    improve name render

--- src/NFT8ZDAORenderer.sol
+++ src/NFT8ZDAORenderer.sol
@@ -1,17 +1,18 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.11;
 
+import "@openzeppelin/contracts/utils/Strings.sol";
 import "@openzeppelin/contracts/utils/Base64.sol";
 
 contract NFT8ZDAORenderer {
-    string public constant description = unicode"天地之间，一起而已。惟有动静，遂分阴阳；有老少，遂分四象。水者，太阴也；火者，太阳也；木者，少阳也；金者，少阴也；土者，阴阳老少，木火金水冲气所结也。——《子平真诠》";
+    using Strings for uint256;
+    string public constant description = unicode"天地之间，一气而已。惟有动静，遂分阴阳；有老少，遂分四象。水者，太阴也；火者，太阳也；木者，少阳也；金者，少阴也；土者，阴阳老少，木火金水冲气所结也。——《子平真诠》";
 
     function renderStrings(
         uint256 tokenId,
         string memory name,
         string[] memory words
     ) external pure returns (string memory) {
-        tokenId;
         string memory svg = string(abi.encodePacked(
             '<svg xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMinYMin meet" viewBox="0 0 350 350">',
             '<style>.t { fill: white; font-family: sans-serif; font-size: ',
@@ -26,7 +27,7 @@ contract NFT8ZDAORenderer {
         svg = string(abi.encodePacked(svg, '</text></svg>'));
         string memory json = Base64.encode(bytes(string(abi.encodePacked(
             '{',
-            '"name":"', name, '",',
+            '"name":"8ZDAO #', tokenId.toString(), ' - ', name, '",',
             '"description":"', description, '",',
             '"image":"data:image/svg+xml;base64,', Base64.encode(bytes(svg)), '"',
             '}'
