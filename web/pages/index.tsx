b96e1cc2b004c3a050905dcda5258f43dd384d24
Ray Victor <ray7551@gmail.com>
2022-07-26 03:11:59 +0800

    add alert for mint error

--- web/pages/index.tsx
+++ web/pages/index.tsx
@@ -10,16 +10,29 @@ import { walletAddressState } from '@/lib/recoil/wallet'
 const Home: NextLayoutPage = () => {
   const [pendingTx, setPendingTx] = useState('')
   const walletAddress = useRecoilValue(walletAddressState)
+  const price = 0.1
 
   const mint = useCallback(async () => {
     const provider = new ethers.providers.Web3Provider((global as any).ethereum)
     const signer = provider.getSigner()
+
+    const balance = await provider.getBalance(await signer.getAddress())
+    const balanceInEth = parseFloat(ethers.utils.formatEther(balance))
+    if (balanceInEth <= price) {
+      alert('You need at least ' + price + 'ETH + gas fee')
+      return
+    }
+
     const erc721 = new ethers.Contract('0xb94fb1122b86d64c034992fd3e6acbf4472d335d', [
       'function mint() public payable',
     ], signer)
-    const tx = await erc721.mint({ value: ethers.utils.parseEther('0.1') })
-    setPendingTx(tx.hash)
-    await tx.wait()
+    try {
+      const tx = await erc721.mint({ value: ethers.utils.parseEther(price + '') })
+      setPendingTx(tx.hash)
+      await tx.wait()
+    } catch (e: any) {
+      alert('Something went wrong.' + (e.code ? 'Error: ' + e.code : ''))
+    }
   }, [])
 
   const Mint = () => {
@@ -29,13 +42,16 @@ const Home: NextLayoutPage = () => {
         <a
           className='underline text-xs'
           href={'https://etherscan.io/tx/' + pendingTx} target='_blank' rel='noreferrer'
-        >{ pendingTx }</a>
+        >{pendingTx}</a>
       </div>
     } else if (walletAddress) {
-      return <button className={clsx(
-        'border-2 border-white hover:border-white/75 hover:text-white/75',
-        'rounded-full px-16 py-2 my-8',
-      )} onClick={() => mint()}>MINT 1 8Z</button>
+      return <>
+        <button className={clsx(
+          'border-2 border-white hover:border-white/75 hover:text-white/75',
+          'rounded-full px-16 py-2 mt-8 mb-2',
+        )} onClick={() => mint()}>MINT 1 8Z</button>
+        <span className='text-xs sm:text-sm'>Ξ{price} + gas fee</span>
+      </>
     } else {
       return <></>
     }
